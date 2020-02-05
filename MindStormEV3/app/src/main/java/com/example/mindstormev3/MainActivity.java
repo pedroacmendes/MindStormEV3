bf02504f750aa86517e348f8b3951211e30b53a5
Pedro Mendes <pedroacmendes@hotmail.com>
2020-02-05 22:41:22 +0000

    Projeto terminado

--- MindStormEV3/app/src/main/java/com/example/mindstormev3/MainActivity.java
+++ MindStormEV3/app/src/main/java/com/example/mindstormev3/MainActivity.java
@@ -34,34 +34,19 @@ import static com.example.mindstormev3.ModoJogo.REQUEST_ENABLE_BT;
 public class MainActivity extends AppCompatActivity {
 
     public BluetoothAdapter mBTAdapter;
-    public ArrayAdapter<String> mBTArrayAdapter;
-    public ListView mDevicesListView;
-    public final static int REQUEST_ENABLE_BT = 1;
-
-    ModoJogo.ConnectedThread mConnectedThread;
-    public Handler mHandler;
-    public BluetoothSocket mBTSocket = null;
-
     Button btnEntrar;
     Button btnSair;
-    Button btnListar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBTArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         mBTAdapter = BluetoothAdapter.getDefaultAdapter();
-        mDevicesListView = (ListView) findViewById(R.id.devicesListView);
-        mDevicesListView.setAdapter(mBTArrayAdapter);
-        //mDevicesListView.setOnItemClickListener(mDeviceClickListener);
-
-        bluetoothOn();
-
         btnEntrar = findViewById(R.id.btnEntrar);
         btnSair = findViewById(R.id.btnSair);
-        btnListar = findViewById(R.id.btnListar);
+
+        bluetoothOn();
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,140 +59,27 @@ public class MainActivity extends AppCompatActivity {
         btnSair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bluetoothOff(v);
+                bluetoothOff();
                 finish();
             }
         });
 
-        btnListar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                discover(v);
-            }
-        });
-
-        mHandler = new Handler() {
-            public void handleMessage(android.os.Message msg) {
-                if (msg.what == MESSAGE_READ) {
-                    String readMessage = null;
-                    try {
-                        readMessage = new String((byte[]) msg.obj, "UTF-8");
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            }
-        };
 
     }
-
 
     private void bluetoothOn() {
         if (!mBTAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             Toast.makeText(getApplicationContext(), "Bluetooth turned on", Toast.LENGTH_SHORT).show();
-
         } else {
             Toast.makeText(getApplicationContext(), "Bluetooth is already on", Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent Data) {
-        super.onActivityResult(requestCode, resultCode, Data);
-    }
-
-    private void bluetoothOff(View view) {
+    private void bluetoothOff() {
         mBTAdapter.disable();
         Toast.makeText(getApplicationContext(), "Bluetooth turned Off", Toast.LENGTH_SHORT).show();
-    }
-
-    private void discover(View view) {
-        if (mBTAdapter.isDiscovering()) {
-            mBTAdapter.cancelDiscovery();
-            Toast.makeText(getApplicationContext(), "Discovery stopped", Toast.LENGTH_SHORT).show();
-        } else {
-            if (mBTAdapter.isEnabled()) {
-                mBTArrayAdapter.clear(); //LIMPA O ARRAY
-                mBTAdapter.startDiscovery();
-                Toast.makeText(getApplicationContext(), "A procurar EV3", Toast.LENGTH_SHORT).show();
-                registerReceiver(blReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-            } else {
-                Toast.makeText(getApplicationContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    final BroadcastReceiver blReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-                mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-                mBTArrayAdapter.notifyDataSetChanged();
-            }
-        }
-    };
-
-    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
-
-            if (!mBTAdapter.isEnabled()) {
-                Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Get the device MAC address, which is the last 17 chars in the View
-            String info = ((TextView) v).getText().toString();
-            final String address = info.substring(info.length() - 17);
-            final String name = info.substring(0, info.length() - 17);
-
-            // Spawn a new thread to avoid blocking the GUI one
-            new Thread() {
-                public void run() {
-                    boolean fail = false;
-
-                    BluetoothDevice device = mBTAdapter.getRemoteDevice(address);
-
-                    try {
-                        mBTSocket = createBluetoothSocket(device);
-                    } catch (IOException e) {
-                        fail = true;
-                        Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
-                    }
-                    // Establish the Bluetooth socket connection.
-
-                    try {
-                        mBTSocket.connect();
-                    } catch (IOException e) {
-                        try {
-                            fail = true;
-                            mBTSocket.close();
-                            mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
-                                    .sendToTarget();
-                        } catch (IOException e2) {
-                            //insert code to deal with this
-                            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    if (fail == false) {
-                        mConnectedThread = new ModoJogo.ConnectedThread(mBTSocket);
-                        mConnectedThread.start();
-
-                        mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
-                                .sendToTarget();
-                    }
-                }
-            }.start();
-        }
-    };
-
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
     }
 
 
